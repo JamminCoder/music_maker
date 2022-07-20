2834f24c1b23549b15bc2e65a30724f612c9d48a
JamminCoder <jammincoderguy96@gmail.com>
2022-07-20 09:33:48 -0400

    Changed chord playing functionality, created `Tone` class

--- Note.py
+++ Note.py
@@ -1,37 +1,34 @@
-from utils import get_note_map, play_tone
+from utils import NOTE_MAP
+from play_tone import Tone
 import threading
 import time
 
-class Note:
-    threads = []
-    def join_threads():
-        for thread in Note.threads:
-            thread.join()
 
+class Note:
     def __init__(self, note, duration=1):
         # Ensures the first character of note is uppercase
-        
-
         main_note = note[0].upper()
         self.note = main_note + note[1:]
         self.duration = duration
-        self.rest = False
-        if note == 'rest':
-            self.rest = True
-            return
-
-        self.note_map = get_note_map()
-        self.frequency = self.note_map[self.note]
+        self.frequency = NOTE_MAP[self.note]
     
     def play(self, speaker=None):
-        if self.rest:
-            time.sleep(self.duration)
-            return
-            
-        play_tone(self.frequency, duration=self.duration, speaker=speaker)
+        Tone.sine(self.frequency, duration=self.duration, speaker=speaker)
 
-    def play_threaded(self):
-        thread = threading.Thread(target=self.play)
-        Note.threads.append(thread)
-        thread.start()
+    @staticmethod
+    def rest(duration):
+        time.sleep(duration)
+
+    @staticmethod
+    def play_chord(notes):
+        threads = []
+        for note in notes:
+            thread = threading.Thread(target=note.play)
+            threads.append(thread)
+        
+        for thread in threads:
+            thread.start()
+        
+        for thread in threads:
+            thread.join()
         
---
+++ play_tone.py
@@ -0,0 +1,61 @@
+import math
+from os import stat
+import numpy
+import time
+import pygame
+import threading
+
+pygame.init()
+bits = 16
+sample_rate = 44100
+pygame.mixer.pre_init(sample_rate, bits)
+
+def sine_x(amp, freq, time):
+    return int(round(amp * math.sin(2 * math.pi * freq * time)))
+
+class Tone:
+    def sine(frequency, duration=1, speaker=None):
+        """
+        Play tone code taken and modified from https://stackoverflow.com/a/16268034
+        """
+
+        num_samples = int(round(duration * sample_rate))
+
+        #setup our numpy array to handle 16 bit ints, which is what we set our mixer to expect with "bits" up above
+        buf = numpy.zeros((num_samples, 2), dtype = numpy.int16)
+        amplitude = 2 ** (bits - 1) - 1
+
+        for s in range(num_samples):
+            t = float(s) / sample_rate    # time in seconds
+
+            sine = sine_x(amplitude, frequency, t)
+
+            # Control which speaker to play the sound from
+            if speaker == 'r':
+                buf[s][1] = sine # right
+            elif speaker == 'l':
+                buf[s][0] = sine # left
+
+            else:
+                buf[s][0] = sine # left
+                buf[s][1] = sine # right
+                
+
+        sound = pygame.sndarray.make_sound(buf)
+        one_sec = 1000 # Milliseconds
+        sound.play(loops = 1, maxtime=int(duration * one_sec))
+        time.sleep(duration)
+    
+    @staticmethod
+    def create_tone_from_array(frequency_array, duration=1):
+        tone_threads = []
+        
+        for freq in frequency_array:
+            thread = threading.Thread(target=Tone.sine, args=[freq, duration])
+            tone_threads.append(thread)
+        
+        for thread in tone_threads:
+            thread.start()
+        
+        for thread in tone_threads:
+            thread.join()
